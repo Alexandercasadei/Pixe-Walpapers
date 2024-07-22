0fb761ab94cbdd93b1e55456d9f547ad8b6c5880
Alexandercasadei <alessiocasadei13@gmail.com>
2024-07-22 17:26:46 +0200

    1 modifica

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function downloadImage(url, filename) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
